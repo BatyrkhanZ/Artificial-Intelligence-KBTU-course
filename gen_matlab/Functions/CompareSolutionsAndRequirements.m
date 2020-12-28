cbbc1f792379bc12ee295a90f55efea0879078d4
BatyrkhanZ <31758911+BatyrkhanZ@users.noreply.github.com>
2020-12-28 20:11:32 +0600

    Add files via upload

---
+++ gen_matlab/Functions/CompareSolutionsAndRequirements.m
@@ -0,0 +1,15 @@
+% A - Solution
+% R - Requirement Matrix
+% D - Individual Department Vector
+
+function f = CompareSolutionsAndRequirements(A,R,D)
+[nj,nk] = size(R);
+
+for j=1:nj
+    index = find(D == j);
+    for k=1:nk
+        Ra(j,k) = sum(A(index,k));
+    end
+end
+
+f = sum(sum(abs(R - Ra)));
